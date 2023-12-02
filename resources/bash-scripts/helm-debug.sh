2d9bd912745d82a918b19d206755e7e9f59bf7b2
k3rcaft <vqt.develop@gmail.com>
2023-12-02 11:01:43 +0700

    added new script: helm-debug

---
+++ resources/bash-scripts/helm-debug.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Purpose: Debug the template will be generated after run it completelly.
+# Notice all variables that are be required.
+# Reference link: https://helm.sh/docs/chart_template_guide/debugging/
+# --------------------------------------
+# Vars of environment
+echo "Stage name: ${SYSTEM_STAGEDISPLAYNAME}"
+echo "Build name: ${RELEASE_DEFINITIONNAME}"
